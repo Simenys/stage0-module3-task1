9d609a48c16f9b84b2b35e99ef1ae56ad64113b8
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-02 19:55:49 +0300

    Task solved

---
+++ src/BrokenApp.java
@@ -0,0 +1,5 @@
+public class BrokenApp {
+        public static void main(String[] args) {
+            System.out.println("\nfix\nMe");
+        }
+}
---
+++ src/UpdateApp.java
@@ -0,0 +1,5 @@
+public class UpdateApp {
+        public static void main(String[] args) {
+            System.out.println("\nI'm updated\ncan be compiled. And can write to console");
+        }
+}
