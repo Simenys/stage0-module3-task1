1c91cceac17bd493b2fb782a2634f9e62cc5fe64
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-02 21:08:00 +0300

    Moving forward

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java" + "\nis" + "\n" + "smth");
+        System.out.println("java");
+        System.out.print("\nis");
+        System.out.print("\n");
+        System.out.print("smth");
     }
 }
