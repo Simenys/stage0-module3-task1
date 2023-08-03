538c10956fb648a7cdf4bc5d74cf9190f118149c
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-03 13:01:59 +0300

    Updated codes

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -3,8 +3,7 @@ package school.mjc.stage0.module3.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
         System.out.println("java");
-        System.out.print("\nis");
-        System.out.print("\n");
-        System.out.print("smth");
+        System.out.println("is");
+        System.out.println("smth");
     }
 }
